57c4d13433e19a9a8c952bc613e7ce5e45f4e3a2
ErHuoChuangTianYa <csu_caotao@126.com>
2018-03-10 15:53:04 +0800

    448: Find All Numbers Disappeared in an Array

---
+++ JS/Array/easy/448_find_all_numbers_disappeared_in_an_array.js
@@ -0,0 +1,51 @@
+//
+// Created by CaoTao, 2018/03/09
+//
+
+/*
+ * LeetCode link:
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+ */
+
+// O(n) runtime without extra space
+var findDisappearedNumbers = function(nums) {
+  var length = nums.length
+  for (var i = 0; i < length; i++) {
+    var destIndex = nums[i] - 1
+    while (nums[i] - 1 !== i && nums[i] !== undefined) {
+      if (nums[i] === nums[nums[i] - 1]) {
+        nums[i] = undefined
+      }
+      var temp = nums[nums[i] - 1]
+      nums[nums[i] - 1] = nums[i]
+      nums[i] = temp
+    }
+  }
+
+  var result = []
+  for (var i = 0; i < length; i++) {
+    if (nums[i] === undefined) {
+      result.push(i + 1)
+    }
+  }
+
+  return result
+};
+
+// O(n) runtime with O(n) extra space
+var findDisappearedNumbers = function(nums) {
+  var length = nums.length, tempArray = new Array(5)
+
+  for (var i = 0; i < length; i++) {
+    tempArray[nums[i] - 1] = nums[i]
+  }
+
+  var result = []
+  for (var i = 0; i < length; i++) {
+    if (tempArray[i] === undefined) {
+      result.push(i + 1)
+    }
+  }
+
+  return result
+}
