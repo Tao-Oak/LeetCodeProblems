b15af470a2562064234ce2eaa194d4bd4e0a8894
ErHuoChuangTianYa <csu_caotao@126.com>
2018-03-12 14:51:48 +0800

    747. Largest Number At Least Twice of Others

---
+++ JS/Array/easy/747_larget_number_at_least_twice_of_others.js
@@ -0,0 +1,31 @@
+//
+// Created by CaoTao, 2018/03/12
+//
+
+/*
+ * LeetCode link:
+ * https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+  var maximunIndex = 0, length = nums.length
+  if (length <= 1) { return -1 }
+  for (var i = 1; i < length; i++) {
+    console.log(nums[i], nums[maximunIndex])
+    if (nums[i] > nums[maximunIndex]) {
+      maximunIndex = i
+    }
+  }
+  console.log(maximunIndex)
+  for (var i = 0; i < length; i++) {
+    if (i === maximunIndex) continue
+    if (nums[i] * 2 > nums[maximunIndex]) {
+      return -1
+    }
+  }
+  return maximunIndex
+};
