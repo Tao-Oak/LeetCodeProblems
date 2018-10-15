653d57687e52bda4e7fc07d1afbaa2bbe5bd8229
操涛 <caotao@meituan.com>
2018-10-15 13:30:10 +0800

    209 minimum size subarray sum

---
+++ Array/medium/209_minimum_size_subarray_sum/MinimumSizeSubarraySum.java
@@ -0,0 +1,76 @@
+//
+// Created by Joshua.cao, 2018/10/13
+//
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+//
+
+// Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+// Example: 
+
+// Input: s = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: the subarray [4,3] has the minimal length under 
+
+
+public class MinimumSizeSubarraySum {
+  public int minSubArrayLen(int s, int[] nums) {
+    int minLength = Integer.MAX_VALUE;
+    int left = 0;
+    int right = left;
+    int sum = 0;
+    while (right < nums.length) {
+      sum += nums[right];
+      if (sum >= s) {
+        minLength = Math.min(minLength, right - left + 1);
+      }
+      while (sum >= s) {
+        sum -= nums[left];
+        left++;
+        if (sum >= s) {
+          minLength = Math.min(minLength, right - left + 1);
+        }
+      }
+      right++;
+    }
+    if (minLength == Integer.MAX_VALUE) {
+      minLength = 0;
+    }
+    return minLength;
+  }
+  
+  // Accepted, beats 7.01%
+  public int minSubArrayLen_2(int s, int[] nums) {
+    int minLen = nums.length + 1;
+    for (int i = 0; i < nums.length; i++) {
+      int subarrayLen = helper(nums, s, i);
+      if (subarrayLen == 1) {
+        minLen = 1;
+        break;
+      }
+      if (subarrayLen < minLen) {
+        minLen = subarrayLen;
+      }
+    }
+    return minLen > nums.length ? 0 : minLen;
+  }
+
+  public int helper(int nums[], int target, int start) {
+    int minLen = nums.length + 1;
+    int sum = 0;
+    for (int i = start; i < nums.length; i++) {
+      sum += nums[i];
+      if (sum >= target) {
+        minLen = i - start + 1;
+        break;
+      }
+    }
+    return minLen;
+  }
+
+  public static void main(String[] args) {
+    MinimumSizeSubarraySum obj = new MinimumSizeSubarraySum();
+    int[] input_1 = {2,3,1,2,4,3};
+    System.out.println(obj.minSubArrayLen(7, input_1));
+  }
+}
