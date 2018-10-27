bf4dc8d1b8749ba5c2f09e9782fcc2f1a60379a1
操涛 <caotao@meituan.com>
2018-10-27 23:48:04 +0800

    729 my calendar i

--- Problems/Array/medium/729_my_calendar_i/MyCalendar.java
+++ Problems/Array/medium/729_my_calendar_i/MyCalendar.java
@@ -3,47 +3,62 @@
 //
 // https://leetcode.com/problems/my-calendar-i/
 //
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class MyCalendar {
   static class Range {
     int start, end;
+    Range left, right;
     Range(int start, int end) {
       this.start = start;
       this.end = end;
     }
   }
 
-  Set<Range> books;
-  public MyCalendar() {
-    books = new TreeSet<>(new Comparator<MyCalendar.Range>() {
-      @Override
-      public int compare(Range o1, Range o2) {
-        return o1.start - o2.start;
-      }
-    });
-  }
+  Range root;
+  public MyCalendar() {}
   
+  // Accepted, beats 57.67%
   public boolean book(int start, int end) {
     if (end < start) return false;
-
-    books.add(new Range(start, end));
+    Range n = insert(root, start, end);
+    if (n == null) return false;
+    root = n;
     return true;
   }
 
-  public void printBooks() {
-    for (Range range: books) {
-      System.out.println("[" + range.start + ", " + range.end + ")");
+  private Range insert(Range parent, int start, int end) {
+    if (parent == null) {
+      return new Range(start, end);
     }
+    if (end <= parent.start) {
+      Range left = insert(parent.left, start, end);
+      if (left == null) return null;
+      parent.left = left;
+    } else if (start >= parent.end) {
+      Range right = insert(parent.right, start, end);
+      if (right == null) return null;
+      parent.right = right;
+    } else {
+      return null;
+    }
+    return parent;
   }
 
   public static void main(String[] args) {
     MyCalendar obj = new MyCalendar();
-    obj.book(10, 20);
-    obj.book(15, 25);
-    obj.book(20, 30);
-    obj.printBooks();
+    int[][] input = {{10, 20}, {15, 25}, {20, 30}};
+    for (int i = 0; i < input.length; i++) {
+      System.out.print(obj.book(input[i][0], input[i][1]) + "\t");
+    }
+    System.out.println();
+
+    MyCalendar obj_2 = new MyCalendar();
+    int[][] input_2 = {
+      {47, 50}, {33, 41}, {39, 45}, {33, 42}, {25, 32},
+      {26, 35}, {19, 25}, { 3,  8}, { 8, 13}, {18, 27}
+    };
+    for (int i = 0; i < input_2.length; i++) {
+      System.out.print(obj_2.book(input_2[i][0], input_2[i][1]) + "\t");
+    }
+    System.out.println();
   }
 }
